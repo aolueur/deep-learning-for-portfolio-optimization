4a0f5cb1f326874e77c9d9c2333a7337e5b5a2cf
yuao <yp439@cornell.edu>
2024-04-23 12:51:53 -0400

    Add equal_weight

--- equal_weight_sharpe.py
+++ equal_weight_sharpe.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def calculate_portfolio_returns(data_train, tickers):
     """
     Calculates the equal-weighted portfolio returns from individual stock returns.
@@ -12,19 +13,23 @@ def calculate_portfolio_returns(data_train, tickers):
     Returns:
     - Series: A pandas Series containing the portfolio returns.
     """
-    tickers_list = tickers.split()  # Ensures we have a list of tickers from a string
+    tickers_list = (
+        tickers.split()
+    )  # Ensures we have a list of tickers from a string
     # Calculate the weights as an array where each ticker has equal weight
-    weights = np.array([1/len(tickers_list)]*len(tickers_list))
+    weights = np.array([1 / len(tickers_list)] * len(tickers_list))
     # Filter the DataFrame for return columns and calculate the portfolio returns using dot product
     portfolio_ret = data_train.filter(like='_Return').dot(weights)
     return portfolio_ret
+
 
 def calculate_sharpe_ratio(portfolio_returns, risk_free_rate=0):
     """
     Calculate the Sharpe Ratio of the portfolio returns.
 
     Parameters:
-    - portfolio_returns (Series): A pandas Series of portfolio returns. It can be obtained from calculate_portfolio_returns function
+    - portfolio_returns (Series): A pandas Series of portfolio returns.
+      It can be obtained from calculate_portfolio_returns function
     - risk_free_rate (float): The risk-free rate of return. Assume risk-free rate is 0 here for simplicity
 
     Returns:
